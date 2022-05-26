18f225f3e09f5bc01da2e75cd4787724b6b704ef
uniquemozilla22 <uniqgaming26@gmail.com>
2022-05-26 21:26:40 +0545

    static react application appended on production

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ const app = express();
 
 app.use(cors());
 
+if (process.env.NODE_ENV === "PRODUCTION") {
+  app.use(express.static(path.join("client/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 app.get("/", (req, res) => res.send("<h1>Server Started</h1>"));
 
 app.listen(process.env.PORT, () => console.log("server running"));
